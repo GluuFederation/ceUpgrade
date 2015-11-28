dd26e932de90c8d79c708fb190a45f9c0f2c99fe
adrian <adrian@gluu.org>
2015-11-28 00:28:05 -0500

    changes for build depends on the release and version

--- build.sh
+++ build.sh
@@ -10,18 +10,32 @@ fi
 
 if [ $VER == 24]; then
 	mkdir -p build/
-	mkdir -p build/opt/gluu-server24/opt/gluu-server-updater/$RELEASE
-	cp -r dist build/opt/gluu-server24/opt/gluu-server-updater/$RELEASE/
-	cp update-24.sh build/opt/gluu-server/opt/gluu-server-updater/$RELEASE/bin/update.sh 
-	cp -r debian24 build/debian
+	mkdir -p build/opt/gluu-server24/opt/gluu-server-updater24/$RELEASE
+
+	cp -r dist build/opt/gluu-server24/opt/gluu-server-updater24/$RELEASE/
+	cp update.sh build/opt/gluu-server24/opt/gluu-server-updater24/$RELEASE/bin/
+	cp -r debian build/debian
+
+	cat build/debian/changelog | sed -e 's/gluu-server-updater/gluu-server-updater24/' > build/debian/changelog
+	cat build/debian/control | sed -e 's/gluu-server-updater/gluu-server-updater24/' > build/debian/control
+	cat build/debian/control | sed -e 's/CHANGEME/gluu-server24/' > build/debian/control
+	cat build/debian/copyright | sed -e 's/gluu-server-updater/gluu-server-updater24/' > build/debian/copyright
 	cat build/debian/postinst | sed -e 's/CHANGEME/$RELEASE/' > build/debian/postinst
+	cat build/debian/rules | sed -e 's/gluu-server-updater/gluu-server-updater24/' > build/debian/rules
+	cat build/opt/gluu-server24/opt/gluu-server-updater24/$RELEASE/bin/update.sh | sed -e 's/CHANGEME/gluu-server24/' > build/opt/gluu-server/opt/gluu-server-updater/$RELEASE/bin/update.sh
+
 else
         mkdir -p build/
         mkdir -p build/opt/gluu-server/opt/gluu-server-updater/$RELEASE/bin
+
         cp -r dist build/opt/gluu-server/opt/gluu-server-updater/$RELEASE/
-	cp update-23.sh build/opt/gluu-server/opt/gluu-server-updater/$RELEASE/bin/update.sh 
-	cp -r debian23 build/debian
+	cp update.sh build/opt/gluu-server/opt/gluu-server-updater/$RELEASE/bin/
+	cp -r debian build/debian
+
+	cat build/debian/control | sed -e 's/CHANGEME/gluu-server/' > build/debian/control
 	cat build/debian/postinst | sed -e 's/CHANGEME/$RELEASE/' > build/debian/postinst
+	cat build/opt/gluu-server/opt/gluu-server-updater/$RELEASE/bin/update.sh | sed -e 's/CHANGEME/gluu-server/' > build/opt/gluu-server/opt/gluu-server-updater/$RELEASE/bin/update.sh
+
 fi
 
 pushd build
--- update.sh
+++ update.sh
@@ -3,15 +3,15 @@
 # need help figureing wich version and files gonna be upgrade it
 
 TOMCATDIRVER="apache-tomcat-7.0.55"
-OXIDP="/opt/gluu-server/opt/idp/war"
-OXCAS="/opt/gluu-server/opt/dist"
-OXTRUST="/opt/gluu-server/opt/$TOMCATDIRVER/webapps"
-OXAUTH="/opt/gluu-server/opt/$TOMCATDIRVER/webapps"
-UPDATER="/opt/gluu-server/opt/gluu-server-updater/$VER/dist"
-GLUU_SERVER="/etc/init.d/gluu-server"
-GLUU_PATH="/opt/gluu-server"
+OXIDP="/opt/CHANGEME/opt/idp/war"
+OXCAS="/opt/CHANGEME/opt/dist"
+OXTRUST="/opt/CHANGEME/opt/$TOMCATDIRVER/webapps"
+OXAUTH="/opt/CHANGEME/opt/$TOMCATDIRVER/webapps"
+UPDATER="/opt/CHANGEME/opt/CHANGEME-updater/$VER/dist"
+GLUU_SERVER="/etc/init.d/CHANGEME"
+GLUU_PATH="/opt/CHANGEME"
 
-echo -e "Beggining of the upgrade process, Stoping gluu-server..."
+echo -e "Beggining of the upgrade process, Stoping CHANGEME..."
 if [ -f $GLUU_SERVER ]; then
 	`$GLUU_SERVER stop`
 fi
@@ -30,7 +30,7 @@ if [ -d $GLUU_PATH ]; then
     		cp $UPDATER/oxauth.war $OXAUTH/oxauth.war
 	fi
 fi
-echo -n "Starting gluu-server..."
+echo -n "Starting CHANGEME..."
 if [ -f $GLUU_SERVER ]; then
       `$GLUU_SERVER start`
 fi
