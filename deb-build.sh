d2ec59db6475a639b08010b23baf0bb2c4717e4d
adrian <adrian@gluu.org>
2015-12-08 08:15:07 -0500

    new scripts to build .deb

--- deb-build.sh
+++ deb-build.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+RELEASE=1
+UPDATER="gluu-updater24"
+
+mkdir -p gluu-updater/opt/$UPDATER/$RELEASE/bin
+cp prepare_update.py gluu-updater/opt/$UPDATER/$RELEASE/bin/.
+cp update.properties gluu-updater/opt/$UPDATER/$RELEASE/bin/.
+cp update.py gluu-updater/opt/$UPDATER/$RELEASE/bin/.
+cp LICENSE gluu-updater/opt/$UPDATER/$RELEASE/.
+cp README.md gluu-updater/opt/$UPDATER/$RELEASE/.
+cp -r dist gluu-updater/opt/$UPDATER/$RELEASE/
+cp -r debian gluu-updater/.
+
 pushd gluu-updater
 debuild clean
 dpkg-buildpackage -us -uc
