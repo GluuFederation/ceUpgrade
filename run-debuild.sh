d2e105668ba96f58b39038e07583885521c98a86
adrian <adrian@gluu.org>
2015-12-23 10:11:57 -0500

    new debian dir

--- run-debuild.sh
+++ run-debuild.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
 RELEASE=1
-UPDATER="gluu-updater24"
+UPDATE="gluu-update24"
 
-mkdir -p gluu-updater/opt/$UPDATER/$RELEASE/bin
-cp update.py gluu-updater/opt/$UPDATER/$RELEASE/bin/.
-cp LICENSE gluu-updater/opt/$UPDATER/$RELEASE/.
-cp README.md gluu-updater/opt/$UPDATER/$RELEASE/.
-cp -r dist gluu-updater/opt/$UPDATER/$RELEASE/
-cp -r debian gluu-updater/.
+mkdir -p gluu-update24/opt/$UPDATE/$RELEASE/bin
+mkdir -p gluu-update24/opt/$UPDATE/$RELEASE/log
+mkdir -p gluu-update24/opt/$UPDATE/$RELEASE/bkp
+cp update.py gluu-update24/opt/$UPDATE/$RELEASE/bin/.
+cp prepare_update.py gluu-update24/opt/$UPDATE/$RELEASE/bin/.
+cp update.properties gluu-update24/opt/$UPDATE/$RELEASE/bin/.
+cp LICENSE gluu-update24/opt/$UPDATE/$RELEASE/.
+cp README.md gluu-update24/opt/$UPDATE/$RELEASE/.
+cp -r dist gluu-update24/opt/$UPDATER/$RELEASE/
+cp -r debian gluu-update24/.
 
-pushd gluu-updater
+pushd gluu-update24
 debuild clean
 dpkg-buildpackage -us -uc
 popd
