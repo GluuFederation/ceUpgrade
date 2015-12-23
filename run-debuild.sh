6ab3540009f381457303f134b69589d810ae1a18
adrian <adrian@gluu.org>
2015-12-23 10:18:08 -0500

    new build script

--- run-debuild.sh
+++ run-debuild.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-RELEASE=1
-UPDATE="gluu-update24"
+#RELEASE=
+#UPDATE="gluu-update24"
 
-mkdir -p gluu-update24/opt/$UPDATE/$RELEASE/bin
-mkdir -p gluu-update24/opt/$UPDATE/$RELEASE/log
-mkdir -p gluu-update24/opt/$UPDATE/$RELEASE/bkp
-cp update.py gluu-update24/opt/$UPDATE/$RELEASE/bin/.
-cp prepare_update.py gluu-update24/opt/$UPDATE/$RELEASE/bin/.
-cp update.properties gluu-update24/opt/$UPDATE/$RELEASE/bin/.
-cp LICENSE gluu-update24/opt/$UPDATE/$RELEASE/.
-cp README.md gluu-update24/opt/$UPDATE/$RELEASE/.
-cp -r dist gluu-update24/opt/$UPDATER/$RELEASE/
+mkdir -p gluu-update24/opt/gluu-server24/var/lib/gluu-update/2.4.1/bin
+mkdir -p gluu-update24/opt/gluu-server24/var/lib/gluu-update/2.4.1/log
+mkdir -p gluu-update24/opt/gluu-server24/var/lib/gluu-update/2.4.1/bkp
+cp update.py gluu-update24/opt/gluu-server24/var/lib/gluu-update/2.4.1/bin/.
+cp LICENSE gluu-update24/opt/gluu-server24/var/lib/gluu-update/2.4.1/.
+cp README.md gluu-update24/opt/gluu-server24/var/lib/gluu-update/2.4.1/.
+rm -rf gluu-update24/opt/gluu-server24/var/lib/gluu-update/2.4.1/dist
+cp -r dist gluu-update24/opt/gluu-server24/var/lib/gluu-update/2.4.1/.
+rm -rf gluu-update24/debian
 cp -r debian gluu-update24/.
 
 pushd gluu-update24
